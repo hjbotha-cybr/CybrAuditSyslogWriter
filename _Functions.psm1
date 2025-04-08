a01b787e120350edc93c43353200b866abc2ac91
Henning Botha <henning.botha@cyberark.com>
2025-04-08 15:55:08 +0100

    documentation on customisable functions

--- _Functions.psm1
+++ _Functions.psm1
@@ -1,4 +1,10 @@
 function ConvertTo-SyslogMessage {
+    <#
+    This function takes the syslog object retrieved from the Audit API and transforms it into an array of strings, which will later be passed to the syslog receiver.
+    This function can be modified to transform the message into a different format as needed.
+    Events must be returned as an array of strings, with each string in the array representing a single event.
+    By default, it converts events into an array of JSON strings (not a JSON array).
+    #>
     Param(
         [PSCustomObject[]]$SyslogMessageObj
     )
